86e1edc980cdadbdad04795a08a58d7a029ba34e
Pablo Martín <goinnn@gmail.com>
2012-07-05 20:22:56 +0200

    Second version to the command

--- README.rst
+++ README.rst
@@ -16,9 +16,20 @@ Installation
     )
 
 
+Requeriments
+============
+
+ * python-graph-core
+ * python-graph-dot
+
+It is possible that the next links are interested if you can some problem:
+
+ * http://code.google.com/p/python-graph/issues/detail?id=15
+ * http://stackoverflow.com/questions/2133767/using-python-graphviz-importerror-no-module-named-gv
+
 Usage
 =====
 
 ::
 
-    python manage.py detect_cyclic | dot -T svg -o my_imports.svg
+    python manage.py detect_cyclic
--- src/django_detect_cyclic/management/commands/detect_cyclic.py
+++ src/django_detect_cyclic/management/commands/detect_cyclic.py
@@ -1,16 +1,34 @@
+
+import sys
+
+try:
+    import gv
+except ImportError:
+    sys.path.append('/usr/lib/pyshared/python2.6')
+    import gv
+
+from pygraph.algorithms.searching import breadth_first_search
+from pygraph.classes.digraph import digraph
+from pygraph.readwrite.dot import write
+
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        first_graph = ('digraph G {'
-                       'ranksep=1.0;'
-                       'node [style=filled,fontname=Helvetica,fontsize=10];'
-                       'App1 -> App2;'
-                       'App1 [label="App1",fillcolor="#f8c85c",peripheries=2];'
-                       'App2 -> App3;'
-                       'App2 [label="App2",fillcolor="#f8c85c"];'
-                       'App3 [label="App3",fillcolor="#f8c85c"];'
-                       '}')
-        print first_graph
+        gr = digraph()
+        gr.add_nodes(["Portugal", "Spain", "France", "Germany", "Belgium", "Netherlands", "Italy"])
+        gr.add_edge(("Portugal", "Spain"))
+        gr.add_edge(("Spain", "Portugal"))
+        gr.add_edge(("Spain", "France"))
+        gr.add_edge(("France", "Belgium"))
+        gr.add_edge(("France", "Germany"))
+        gr.add_edge(("France", "Italy"))
+        gr.add_edge(("Belgium", "Netherlands"))
+        gr.add_edge(("Germany", "Belgium"))
+        gr.add_edge(("Germany", "Netherlands"))
+        dot = write(gr)
+        gvv = gv.readstring(dot)
+        gv.layout(gvv, 'dot')
+        gv.render(gvv, 'svg', 'europe3.svg')
