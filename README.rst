5310ad9dbba251512607594896fbad32a8d1d542
Pablo Martín <goinnn@gmail.com>
2012-07-05 19:40:39 +0200

    The first graph

--- README.rst
+++ README.rst
@@ -1,4 +1,24 @@
-django-detect-cyclic
+====================
+Django detect cyclic
 ====================
 
-Django detect cyclic
+Application to detect cyclic imports
+
+Installation
+============
+
+::
+
+    INSTALLED_APPS = (
+
+        'django_detect_cyclic',
+
+    )
+
+
+Usage
+=====
+
+::
+
+    python manage.py detect_cyclic | dot -T svg -o my_imports.svg
--- src/django_detect_cyclic/management/commands/detect_cyclic.py
+++ src/django_detect_cyclic/management/commands/detect_cyclic.py
@@ -4,4 +4,13 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        print 'test'
+        first_graph = ('digraph G {'
+                       'ranksep=1.0;'
+                       'node [style=filled,fontname=Helvetica,fontsize=10];'
+                       'App1 -> App2;'
+                       'App1 [label="App1",fillcolor="#f8c85c",peripheries=2];'
+                       'App2 -> App3;'
+                       'App2 [label="App2",fillcolor="#f8c85c"];'
+                       'App3 [label="App3",fillcolor="#f8c85c"];'
+                       '}')
+        print first_graph
